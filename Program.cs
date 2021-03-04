89fe6484cb6a757ec2334c8f47553b37b71dc88c
Glipotions <hamzakavak1905@gmail.com>
2021-03-04 09:06:55 +0300

    MyDictionary

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyDictionary<int, string> dictionary1 = new MyDictionary<int, string>();
+
+			dictionary1.Add(1, "Hamza");
+			dictionary1.Add(2, "Deres");
+			dictionary1.Add(2, "Salata");
+			dictionary1.Listele();
+
+		}
+	}
+}
